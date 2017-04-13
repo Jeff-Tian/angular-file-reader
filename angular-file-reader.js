86ea7713199c89e7e6f35c6a948f339eadcb2f6e
叽歪 <jeff.tian@outlook.com>
2017-04-13 14:20:34 +0800

    Add rquest transformers.

--- angular-file-reader.js
+++ angular-file-reader.js
@@ -1,4 +1,37 @@
 angular.module('angular-file-reader', [])
+    .service('requestTransformers', [function () {
+        this.transformToFormData = function (data, getHeaders) {
+            function appendFormData(formData, key, value) {
+                if (value instanceof window.File) {
+                    formData.append(key, value, value.name);
+                    return;
+                }
+
+                if (value instanceof window.Blob) {
+                    formData.append(key, value, key + '.png');
+                    return;
+                }
+
+                if (typeof value !== 'undefined') {
+                    formData.append(key, value);
+                    return;
+                }
+            }
+
+            var formData = new window.FormData();
+            angular.forEach(data, function (value, key) {
+                if (value instanceof Array) {
+                    for (var i = 0; i < value.length; i++) {
+                        appendFormData(formData, key + '[' + i + ']', value[i]);
+                    }
+                } else {
+                    appendFormData(formData, key, value);
+                }
+            });
+
+            return formData;
+        };
+    }])
     .directive('fileread', [function () {
         return {
             scope: {
--- dist/gulpfile.js
+++ dist/gulpfile.js
@@ -1,11 +1,1 @@
-'use strict';
-
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-
-gulp.task('uglify-js', function (done) {
-    return gulp.src('dist/**/*.js').pipe(uglify()).pipe(gulp.dest('dist'));
-});
-
-gulp.task('default', ['uglify-js']);
-//# sourceMappingURL=gulpfile.js.map
+"use strict";var gulp=require("gulp"),uglify=require("gulp-uglify");gulp.task("uglify-js",function(u){return gulp.src("dist/**/*.js").pipe(uglify()).pipe(gulp.dest("dist"))}),gulp.task("default",["uglify-js"]);
